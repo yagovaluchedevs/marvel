b1ae1e8b5718c6663ab2f9704c1d5b7a5b6893fe
Yago Valuche <valuchedevs@gmail.com>
2021-01-08 18:20:59 -0300

    tentando resolver erro

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-// import CharactersColletion from "./page/CharactersCollection";
+import CharactersColletion from "./page/CharactersCollection";
 import AboutTheCharacter from "./page/AboutTheCharacter";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route exact path="/" component={CharactersColletion} /> */}
+        <Route exact path="/" component={CharactersColletion} />
         <Route
           exact
           path="/detalhe-do-personagem/:id"
